730eed5c842d0e726553f3a04683d48677dc192b
Crowdbotics <team@crowdbotics.com>
2023-07-10 00:46:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lingering-dust-42030",
+  url: "https://lingering-dust-42030.botics.co",
+  api: "https://lingering-dust-42030.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
